a030fe27e201d5f639a5bb3f74d9bc3504e799c9
KrisAsante <christopher.asante@ocsbstudent.ca>
2018-02-09 13:10:59 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,14 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- Created by: Chris Asante
+-- Created on: Feb 2017
+-- This displays colour to the tablet
+--
+-----------------------------------------------------------------------------------------
+
+display.setDefault("background", 0, 0, 1 )
+local image = display.newImageRect( "Attack__005.png", 1000, 1000 )
+image.x = 1000
+image.y = 1000
